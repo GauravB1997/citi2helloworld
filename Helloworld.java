0ae5cd411cb065347f85e2811e131520a6116f3f
gaurav <gauravbhandari1997@gmail.com>
2020-09-16 11:37:24 +0000

    new Hello

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,13 @@
-class Helloworld {
+//Simple Java Program to Print Hello World
+public class Helloworld {
 
-public static void main(String args[]){
-System.out.println("hello world experimental");
-}
+	public static void main(String[] args)
+	{
+		int i;
+		for(i = 0; i <= 5; i++)
+		{
+			System.out.println("\n Hello World ");
+		}	
+	}
 }
 
